a63f00a120c8ae40bff744f67b75ef1abbae83ef
Jaeden Rotondo <jae.rotondo@hotmail.ca>
2025-05-29 14:11:29 -0400

    restart

--- src/yjs/provider.ts
+++
@@ -1,5 +0,0 @@
-import { WebrtcProvider } from 'y-webrtc';
-import { ydoc } from './ydoc';
-
-const roomName = `overleaf-per-room-${crypto.randomUUID()}`;
-export const provider = new WebrtcProvider(roomName, ydoc);
--- src/yjs/ydoc.ts
+++
@@ -1,4 +0,0 @@
-import * as Y from 'yjs';
-
-export const ydoc = new Y.Doc();
-export const ytext = ydoc.getText('document');
